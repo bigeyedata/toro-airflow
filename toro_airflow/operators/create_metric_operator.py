d859791f7676b5e7fc922ea6afae556bfc8eff3a
egor <egor@torodata.io>
2021-07-22 13:59:33 -0700

    Create different metric types

--- toro_airflow/operators/create_metric_operator.py
+++ toro_airflow/operators/create_metric_operator.py
@@ -8,7 +8,7 @@ from airflow.operators.bash_operator import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
 
-class UpsertFreshnessMetricOperator(BaseOperator):
+class CreateMetricOperator(BaseOperator):
 
     # Only for Python 3.8+
     # TODO - find a way to check what Python version is running
@@ -26,11 +26,11 @@ class UpsertFreshnessMetricOperator(BaseOperator):
                  connection_id: str,
                  warehouse_id: int,
                  configuration: list(dict(schema_name=None, table_name=None, column_name=None,
-                                          update_schedule=None,
+                                          update_schedule=None, metric_name=None,
                                           extras=...)),
                  *args,
                  **kwargs):
-        super(UpsertFreshnessMetricOperator, self).__init__(*args, **kwargs)
+        super(CreateMetricOperator, self).__init__(*args, **kwargs)
         self.connection_id = connection_id
         self.warehouse_id = warehouse_id
         self.configuration = configuration
@@ -41,50 +41,48 @@ class UpsertFreshnessMetricOperator(BaseOperator):
             schema_name = c["schema_name"]
             column_name = c["column_name"]
             default_check_frequency_hours = c.get("default_check_frequency_hours", 2)
-            update_schedule = c["update_schedule"]
+            update_schedule = c.get("update_schedule", None)
             delay_at_update = c.get("delay_at_update", "0 minutes")
             timezone = c.get("timezone", "UTC")
             notifications = c.get("notifications", [])
+            metric_name = c.get("metric_name")
+            should_backfill = c.get("should_backfill", False)
+            if metric_name is None:
+                raise Exception("Metric name must be present in configuration", c)
             table = self._get_table_for_name(schema_name, table_name)
             if table is None or table.get("id") is None:
                 raise Exception("Could not find table: ", schema_name, table_name)
-            existing_metric = self._get_existing_freshness_metric(table, column_name)
+            existing_metric = self._get_existing_metric(table, column_name, metric_name)
             metric = self._get_metric_object(existing_metric, table, notifications, column_name,
-                                             update_schedule, delay_at_update, timezone, default_check_frequency_hours)
+                                             update_schedule, delay_at_update, timezone, default_check_frequency_hours,
+                                             metric_name)
             logging.info("Sending metric to create: %s", metric)
-            hook = self.get_hook('POST')
-            result = hook.run("api/v1/metrics",
-                              headers={"Content-Type": "application/json", "Accept": "application/json"},
-                              data=json.dumps(metric))
+            bigeye_post_hook = self.get_hook('POST')
+            result = bigeye_post_hook.run("api/v1/metrics",
+                                          headers={"Content-Type": "application/json", "Accept": "application/json"},
+                                          data=json.dumps(metric))
             logging.info("Create metric status: %s", result.status_code)
             logging.info("Create result: %s", result.json())
+            if should_backfill and result.json().get("id") is not None:
+                bigeye_post_hook.run("/api/v1/metrics/backfill",
+                                     headers={"Content-Type": "application/json", "Accept": "application/json"},
+                                     data=json.dumps({"metricIds": [result.json()["id"]]}))
 
     def get_hook(self, method) -> HttpHook:
         return HttpHook(http_conn_id=self.connection_id, method=method)
 
     def _get_metric_object(self, existing_metric, table, notifications, column_name,
-                           update_schedule, delay_at_update, timezone, default_check_frequency_hours):
-        metric_name = self._get_metric_name_for_field(table, column_name)
+                           update_schedule, delay_at_update, timezone, default_check_frequency_hours, metric_name):
+        if self._is_freshness_metric(metric_name):
+            metric_name = self._get_freshness_metric_name_for_field(table, column_name)
+            if update_schedule is None:
+                raise Exception("Update schedule can not be null for freshness schedule thresholds")
         metric = {
             "scheduleFrequency": {
                 "intervalType": "HOURS_TIME_INTERVAL_TYPE",
                 "intervalValue": default_check_frequency_hours
             },
-            "thresholds": [
-                {
-                    "freshnessScheduleThreshold": {
-                        "bound": {
-                            "boundType": "UPPER_BOUND_SIMPLE_BOUND_TYPE",
-                            "value": -1
-                        },
-                        "cron": update_schedule,
-                        "timezone": timezone,
-                        "delayAtUpdate": self._get_time_interval_for_delay_string(delay_at_update,
-                                                                                  metric_name,
-                                                                                  update_schedule)
-                    }
-                }
-            ],
+            "thresholds": self._get_thresholds_for_metric(metric_name, timezone, delay_at_update, update_schedule),
             "warehouseId": self.warehouse_id,
             "datasetId": table.get("id"),
             "metricType": {
@@ -100,8 +98,11 @@ class UpsertFreshnessMetricOperator(BaseOperator):
             ],
             "lookback": {
                 "intervalType": "DAYS_TIME_INTERVAL_TYPE",
-                "intervalValue": 14
+                "intervalValue": 1
             },
+            # 24 hour window mexis metric
+            "lookbackType": "METRIC_TIME_LOOKBACK_TYPE",
+            "grainSeconds": 86400,
             "notificationChannels": self._get_notification_channels(notifications)
         }
         if existing_metric is None:
@@ -112,7 +113,34 @@ class UpsertFreshnessMetricOperator(BaseOperator):
             existing_metric["scheduleFrequency"] = metric["scheduleFrequency"]
             return existing_metric
 
-    def _get_existing_freshness_metric(self, table, column_name):
+    def _is_freshness_metric(self, metric_name):
+        return "HOURS_SINCE_MAX" in metric_name
+
+    def _get_thresholds_for_metric(self, metric_name, timezone, delay_at_update, update_schedule):
+        # Current path for freshness
+        if self._is_freshness_metric(metric_name):
+            return [{
+                "freshnessScheduleThreshold": {
+                    "bound": {
+                        "boundType": "UPPER_BOUND_SIMPLE_BOUND_TYPE",
+                        "value": -1
+                    },
+                    "cron": update_schedule,
+                    "timezone": timezone,
+                    "delayAtUpdate": self._get_time_interval_for_delay_string(delay_at_update,
+                                                                              metric_name,
+                                                                              update_schedule)
+                }
+            }]
+        # Default to autothresholds
+        return [
+            {"autoThreshold": {"bound": {"boundType": "LOWER_BOUND_SIMPLE_BOUND_TYPE", "value": -1.0},
+                               "modelType": "UNDEFINED_THRESHOLD_MODEL_TYPE"}},
+            {"autoThreshold": {"bound": {"boundType": "UPPER_BOUND_SIMPLE_BOUND_TYPE", "value": -1.0},
+                               "modelType": "UNDEFINED_THRESHOLD_MODEL_TYPE"}}
+        ]
+
+    def _get_existing_metric(self, table, column_name, metric_name):
         hook = self.get_hook('GET')
         result = hook.run("api/v1/metrics?warehouseIds={warehouse_id}&tableIds={table_id}"
                           .format(warehouse_id=self.warehouse_id,
@@ -120,17 +148,17 @@ class UpsertFreshnessMetricOperator(BaseOperator):
                           headers={"Accept": "application/json"})
         metrics = result.json()
         for m in metrics:
-            if self._is_latency_metric(m) and self._is_same_column_metric(m, column_name):
+            if self._is_same_type_metric(m, metric_name) and self._is_same_column_metric(m, column_name):
                 return m
         return None
 
     def _is_same_column_metric(self, m, column_name):
         return m["parameters"][0].get("columnName").lower() == column_name.lower()
 
-    def _is_latency_metric(self, metric):
+    def _is_same_type_metric(self, metric, metric_name):
         keys = ["metricType", "predefinedMetric", "metricName"]
         result = reduce(lambda val, key: val.get(key) if val else None, keys, metric)
-        return result is not None and result.startswith("HOURS_SINCE_MAX")
+        return result is not None and result == metric_name
 
     def _get_table_for_name(self, schema_name, table_name):
         hook = self.get_hook('GET')
@@ -153,7 +181,7 @@ class UpsertFreshnessMetricOperator(BaseOperator):
                 channels.append({"email": n})
         return channels
 
-    def _get_metric_name_for_field(self, table, column_name):
+    def _get_freshness_metric_name_for_field(self, table, column_name):
         for f in table.get("fields"):
             if f.get("fieldName").lower() == column_name.lower():
                 if f.get("type") == "TIMESTAMP_LIKE":
