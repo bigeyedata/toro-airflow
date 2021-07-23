62dcd610001cdc47595df84a90ada277477517ec
egor <egor@torodata.io>
2021-07-23 10:28:07 -0400

    Updating metric run operator to take list of metric ids

--- toro_airflow/operators/run_metrics_operator.py
+++ toro_airflow/operators/run_metrics_operator.py
@@ -24,20 +24,25 @@ class RunMetricsOperator(BaseOperator):
         self.metric_ids = metric_ids
 
     def execute(self, context):
-        table = self._get_table_for_name(self.schema_name, self.table_name)
-        if table is None or table.get("id") is None:
-            raise Exception("Could not find table: ", self.schema_name, self.table_name)
-        table_id = table.get("id")
+        metric_ids_to_run = []
         hook = self.get_hook('GET')
-        result = hook.run("api/v1/metrics?warehouseIds={warehouse_id}&tableIds={table_id}"
-                          .format(warehouse_id=self.warehouse_id,
-                                  table_id=table_id),
-                          headers={"Accept": "application/json"})
-        metrics = result.json()
+        if self.metric_ids is None:
+            table = self._get_table_for_name(self.schema_name, self.table_name)
+            if table is None or table.get("id") is None:
+                raise Exception("Could not find table: ", self.schema_name, self.table_name)
+            table_id = table.get("id")
+            result = hook.run("api/v1/metrics?warehouseIds={warehouse_id}&tableIds={table_id}"
+                              .format(warehouse_id=self.warehouse_id,
+                                      table_id=table_id),
+                              headers={"Accept": "application/json"})
+            metrics = result.json()
+            metric_ids_to_run = [m['id'] for m in metrics]
+        else:
+            metric_ids_to_run = self.metric_ids
         num_failing_metrics = 0
-        for m in metrics:
+        for m in metric_ids_to_run:
             logging.debug("Running metric: %s", m)
-            metric_result = hook.run("statistics/runOne/{id}".format(id=m['id'])).json()
+            metric_result = hook.run("statistics/runOne/{id}".format(id=m)).json()
             for mr in metric_result:
                 if not mr['statusOk']:
                     logging.error("Metric is not OK: %s", m)
